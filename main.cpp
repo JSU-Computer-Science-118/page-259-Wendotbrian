160a0b9bdee326a73026c499e03a7c10eb1b63ca
Wendotbrian <97192618+Wendotbrian@users.noreply.github.com>
2022-11-11 19:54:01 -0600

    Quadratic formula

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
-// Brian Koringo #j00962107  11/04/2022
+// Brian Koringo #j00962107  11/04/2022\
+//Quadratic formula
+//This program calculates accepts the input of coefficients of the quadratic equation, calculates the discriminant and outputs the type of roots the equation has also outputs the roots of the equation if the discriminant is >=0
 #include <iostream>
 #include <string>
 #include <cmath>
@@ -7,10 +9,11 @@ using namespace std;
 int main()
 {
 double a, b, c, discriminant, root1, root2;
+  cout << "Input: ";
   cin >> a;
   if (a==0)
   {
-    cout << "\n" << "Invalid Entry!";
+    cout << "\n" << "Output: " << "Invalid Entry!";
   }
   else
   {
@@ -23,19 +26,19 @@ root2=(-b-discriminant)/(2*a);   //finds the roots  of the equation
 }
   if (discriminant > 0)
   {
-    cout << "Two real roots" << endl;
+    cout << "Output:" << "\n" << "Two real roots" << endl;
     cout << "- First root " << root1 << endl;
     cout << "- Second root " << root2 << endl;
     cout << endl;
   }
   else if (discriminant < 0)
   {
-    cout << "Complex roots";
+    cout << "Output: "<< "Complex roots";
     cout << endl;
   }
   else
   {
-    cout << "The single root is " << root1;
+    cout << "Output: " << "The single root is " << root1;
     cout << endl;
   }
   return 0;
